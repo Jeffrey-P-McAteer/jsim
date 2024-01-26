431ff32434fbff289be9edd4b2aeda98313d1e1d
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-01-25 20:32:22 -0500

    js -> rust UI stuff

--- jsim/src/main.rs
+++ jsim/src/main.rs
@@ -9,12 +9,17 @@ fn main() {
         .title("My Project")
         .content(web_view::Content::Html(html_content))
         .size(320, 480)
-        .resizable(false)
+        .resizable(true)
         .debug(true)
         .user_data(())
-        .invoke_handler(|_webview, _arg| Ok(()))
+        //.invoke_handler(|_webview, _arg| Ok(()))
+        .invoke_handler(invoke_handler)
         .run()
         .unwrap();
 }
 
+fn invoke_handler<T>(webview: &mut web_view::WebView<T>, arg: &str) -> Result<(), web_view::Error> {
+  println!("invoke_handler run, arg: {:?}", arg);
 
+  Ok(())
+}
